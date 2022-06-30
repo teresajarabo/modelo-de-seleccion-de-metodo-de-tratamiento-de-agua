b4bcae6ac0682026b994bf044961fcd3e271f230
teresajarabo <107311031+teresajarabo@users.noreply.github.com>
2022-06-30 12:33:11 -0400

    Delete pages directory

--- pages/1_Base_de_datos.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.subheader('Base de datos del modelo')
-df = pd.read_csv('model_data.csv') # Base de datos
-
-st.table(df)
--- pages/3_Tabla_comparativa.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-df_comparativa = pd.read_csv('tabla_comparativa.csv')
-
-st.header('Tabla comparativa')
-st.table(df_comparativa)
-st.caption('El caudal está en litros por minuto')
